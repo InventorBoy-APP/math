6c0adf73b0e44dde39e9560de593ad6b3619416b
InventorBoy-APP <78768396+InventorBoy-APP@users.noreply.github.com>
2021-08-23 20:46:47 -0400

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -16,33 +16,24 @@ document.getElementById("player_answer").innerHTML="Answer Turn-"+player2_name;
 
 
 function check(){
-    getWord=document.getElementById("word").value;
-    word=getWord.toLowerCase();
-    console.log("wordinLowerCase"+word);
-
-    charat1=word.charAt(1);
-    lengthby2=Math.floor(word.length/2);
-    charat2=word.charAt(lengthby2);
-    lengthminus1=word.length-1;
-    charat3=word.charAt(lengthminus1);
-
-    remove_charat1=word.replace(charat1,"_");
-    remove_charat2=remove_charat1.replace(charat2,"_");
-    remove_charat3=remove_charat2.replace(charat3,"_");
-    console.log(remove_charat3);
-    question_word = "<h4 id='word_display'> Q. "+remove_charat3+"</h4>";
+    number1=document.getElementById("word").value;
+    number2=document.getElementById("wordtwo").value;
+    actual_answer=parseInt(number1)*parseInt(number2);
+    console.log(actual_answer);
+    questionNumber="<h4>"+number1+"X"+number2+"</h4>";
     input_box = "<br>Answer : <input type='text' id='input_check_box'>";
-    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
-    row=question_word+input_box+check_button;
+    check_button = "<br><br><button class='btn btn-info' onclick='check1()'>Check</button>";
+    row=questionNumber+input_box+check_button;
     document.getElementById("output").innerHTML=row;
     document.getElementById("word").value="";
-}
+    document.getElementById("wordtwo").value="";
 
+}
+question_turn="player1";
+answer_turn="player2";
 function check1(){
     get_answer=document.getElementById("input_check_box").value;
-    answer=get_answer.toLowerCase();
-    console.log("answer in lowercase-"+answer);
-    if(answer==word){
+    if(get_answer==actual_answer){
         if(answer_turn=="player1"){
             player1_score=player1_score+1;
             document.getElementById("player1_score").innerHTML=player1_score;
@@ -71,5 +62,6 @@ function check1(){
     }
     document.getElementById("output").innerHTML="";
 }
+
 
 
